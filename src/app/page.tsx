37a3990693801a4b7556b3ee4fcea0670fe3e4cc
gwigz <gwigz@users.noreply.github.com>
2024-05-11 09:08:24 +0100

    feat: better mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ dayjs.extend(relativeTime);
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen max-w-4xl mx-auto flex-col gap-12 items-center justify-between text-balance text-center text-xl p-24">
-			<h1 className="text-8xl font-semibold">NO</h1>
+		<main className="flex min-h-screen max-w-4xl mx-auto flex-col gap-8 sm:gap-12 sm:items-center justify-between text-balance sm:text-center text-xl p-6 sm:p-24">
+			<h1 className="text-9xl font-semibold">NO</h1>
 			<h2 className="text-xl max-w-3xl font-bold">
 				Firestorm is not PBR ready yet,{" "}
 				<span className="text-blue-200">Linden Lab</span> released their PBR
@@ -24,6 +24,7 @@ export default function Home() {
 					{dayjs().to(new Date("2023-12-24"))}
 				</a>
 			</h3>
+			<hr className="w-full border-t-2 opacity-20 my-4" />
 			<div className="space-y-8 leading-relaxed font-light">
 				<p>
 					Instead of just slapping on textures and hoping for the best, PBR uses
@@ -35,14 +36,14 @@ export default function Home() {
 					Games like Cyberpunk 2077, Fortnite, and Halo all use PBR to great
 					effect, drawing you in to their worlds with immersive visuals. Now we
 					have this in Second Life. Don't worry about your system performance
-					either. PBR has been making your games look cool since 2013. It's a
+					either, PBR has been making your games look cool since 2013. It's a
 					well understood technology and it's more than likely your graphics
 					card is more than ready for it.
 				</p>
 			</div>
 			<div className="flex flex-col gap-6">
 				<h3 className="font-semibold">Try a PBR viewer today!</h3>
-				<div className="flex [&>*]:flex-grow gap-12">
+				<div className="flex flex-col sm:flex-row [&>*]:flex-grow gap-4 sm:gap-12">
 					<a href="https://secondlife.com/support/downloads" rel="noreferrer">
 						Official Viewer
 					</a>
@@ -62,14 +63,17 @@ export default function Home() {
 						it's a little hard to find on their site, so follow the guide below
 					</span>
 				</h3>
-				<ol className="list-decimal text-left max-w-lg mx-auto font-light leading-relaxed">
+				<ol className="list-decimal text-left max-w-lg mx-auto font-light leading-relaxed pl-6 space-y-4">
 					<li>
 						Join the Firestorm{" "}
 						<a href="secondlife:///app/group/7ba4569c-9dd9-fed2-aaa7-36065d18a13c/about">
 							preview viewer group
 						</a>
 					</li>
-					<li>Check the group notices for the latest beta viewer</li>
+					<li>
+						Check the <span className="font-semibold">group notices</span> for
+						the latest beta viewer
+					</li>
 					<li>Download and install the beta viewer</li>
 				</ol>
 			</div>
